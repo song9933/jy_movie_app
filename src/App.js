15fc1769fc8347b24e5ade8c891c1fe84563d83a
Your Name <sngbrm99@gmail.com>
2020-07-13 18:18:52 +0900

    movie app settings

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
-function App() {
-  return (
-    <div><h1>hello</h1></div>
-  )
+class App extends React.Component {
+  state = {
+    isLoading : true,
+    movies : []
+  };
+  getMovies = async () => {
+    const {data: {data:{movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating")
+    this.setState({movies, isLoading: false});
+  };
+  componentDidMount(){
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies} = this.state;
+  return <div>{isLoading ? "Loading.." : movies.map(movie => {
+    return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image}/>
+  })}</div>;
+  }
 }
 
 export default App;
